b7b140939192586aba7936fceaa5b3f2309ca077
Bossn <yoyoyoyo894@gmail.com>
2021-07-26 11:32:40 +0800

    update

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '好抱歉,您說什麼'
+    r = '很抱歉,您說什麼'
 
     if msg == 'hi':
         r = 'hi'
@@ -46,7 +46,7 @@ def handle_message(event):
         r = '還沒'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
